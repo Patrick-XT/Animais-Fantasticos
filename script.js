d9388fce48b9dc762bf4dcf6eb0111a6cd260e65
patrick <patrickbrasilr@gmail.com>
2022-02-11 13:08:45 -0300

    Accordion list

--- script.js
+++ script.js
@@ -20,4 +20,22 @@ function initTabNav() {
   }
 }
 
-initTabNav()
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
