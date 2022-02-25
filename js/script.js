187328fec6c9bf9673f86b5e8a0674cccc627976
patrick <patrickbrasilr@gmail.com>
2022-02-25 15:52:18 -0300

    module

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import initScrollSuave from "./module/scroll-suave.js";
+import initAnimacaoScroll from "./module/animation-scroll.js"
+import initTabNav from "./module/tab-nav.js"
+import initAccordion from "./module/accordion.js"
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initTabNav();
+initAccordion()
+
+
