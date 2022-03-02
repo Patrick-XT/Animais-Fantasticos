ea1521585701a20bc8a089acb86fb28e5995c82a
patrick <patrickbrasilr@gmail.com>
2022-03-02 15:10:15 -0300

    modal login

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ import initScrollSuave from "./module/scroll-suave.js";
 import initAnimacaoScroll from "./module/animation-scroll.js"
 import initTabNav from "./module/tab-nav.js"
 import initAccordion from "./module/accordion.js"
+import initModal from "./module/modal.js"
+
 
 
 initScrollSuave();
 initAnimacaoScroll();
 initTabNav();
 initAccordion()
-
+initModal()
 
